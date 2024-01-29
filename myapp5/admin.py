81cea822651f82586afe608958a77c827fc6514b
oksanaverkh <oksana-tyryatkina@yandex.ru>
2024-01-29 18:05:40 +0300

    final commit

---
+++ myapp5/admin.py
@@ -0,0 +1,48 @@
+from django.contrib import admin
+from .models import Category, Product
+
+
+@admin.action(description="Сбросить количество в ноль")
+def reset_quantity(modeladmin, request, queryset):
+    queryset.update(quantity=0)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'quantity']
+    ordering = ['category', '-quantity']
+    list_filter = ['date_added', 'price']
+    search_fields = ['description']
+    search_help_text = 'Поиск по полю Описание продукта (description)'
+    actions = [reset_quantity]
+
+    # fields = ['name', 'description', 'category', 'date_added', 'rating']
+    readonly_fields = ['date_added', 'rating']
+
+    fieldsets = [
+        (
+            'Author',
+            {
+                'classes': ['wide'],
+                'fields': ['name', 'last_name'],
+            },
+        ),
+        (
+            'Подробности',
+            {
+                'classes': ['collapse'],
+                'description': 'Категория товара и его подробное описание',
+                'fields': ['category', 'description'],
+            },
+        ),
+        (
+            'Other',
+            {
+                'description': 'Контактная информация',
+                'fields': ['email'],
+            }
+        ),
+    ]
+
+
+admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
